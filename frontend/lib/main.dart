321013ac933212bb6fbd25f46cb621d233f3224d
Lilian Pletosu <lilianpletosu7@gmail.com>
2024-05-09 14:28:13 +0300

    was added new styles and changed docker settings
    1. Was added all colors, texts, labels and buttons styles from Figma.
    1. Was changed docker settings. For open project with new settings please read README.md

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:erp_frontend_v2/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'graphql/graphql_client.dart';
 
 void main() {
   runApp(
