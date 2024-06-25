73147631b80144adae6e1ae6641e8d1f6561864e
neobyte <georgiu@neobytesolutions.com>
2024-06-25 15:54:56 +0300

    Erp 8 dev.env create testing live environment (#36)
    * first commit with new changes for creating live and dev environment
    
    * change flutter channel to beta
    
    * change flutter channel to beta
    
    * graphql
    
    * chore: Update Flutter plugin registration for ConnectivityPlusPlugin
    
    ---------
    
    Co-authored-by: Pletosu Lilian <lilianpletosu7@gmail.com>

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,7 +16,7 @@ import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
